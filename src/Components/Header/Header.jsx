7427af48dc8b9538ef0fa82f792b4c6b80cdfc6e
ThiagobCoelho <tbarroscoelho@gmail.com>
2023-07-13 17:49:44 -0300

    fix: fix bug for netlify error

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = ({focus}) => {
         <section className="Header">
             <ul className="Header__menu-itens">
                 <li className={focus=="organizations"?`Header__menu-itens--focus`:null}><a href="/">Organização</a></li>
-                <li className={focus=="tasks"?`Header__menu-itens--focus`:null}><a href="/tasks">Tarefas</a></li>
+                <li className={focus=="tasks"?`Header__menu-itens--focus`:null}><a href="/">Tarefas</a></li>
                 
             </ul>
         </section>
