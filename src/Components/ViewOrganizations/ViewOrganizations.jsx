839c29138c908daaf8d62caccaa71dd281e6279f
ThiagobCoelho <tbarroscoelho@gmail.com>
2023-07-13 10:54:06 -0300

    feat: implement delete groups functionality

--- src/Components/ViewOrganizations/ViewOrganizations.jsx
+++ src/Components/ViewOrganizations/ViewOrganizations.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import del from "../../Assets/delete.svg";
 
 const ViewOrganizations = ({ organizations, deleteOrganization }) => {
     return (
         <>
             {
                 organizations.length === 0 &&
-                    "Sem grupos de tarefas, adicione um novo" /* ShorCircuit */
+                    <div className="OrganizationList__items">Sem grupos de tarefas, adicione um novo</div>
             }
             {organizations.map((organization) => {
-                return <>
-                    <div className="OrganizationList__items">{organization.title}</div>
-                </>;
+                return (
+                    <>
+                        <div
+                            key={organization.id}
+                            className="OrganizationList__items"
+                        >
+                            {organization.title}
+                            <img
+                                src={del}
+                                onClick={() => deleteOrganization(organization.id)}
+                                width={"25px"}
+                            />
+                        </div>
+                    </>
+                );
             })}
         </>
     );
